9680e53df56197a795305b68ccee42f7aaa8d82a
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-03-05 22:50:54 +0530

    new start

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import TopNavbar from "./Components/TopNavbar";
-import { Container } from "@chakra-ui/react";
 import Body from "./Components/Body";
 
 function App() {
   return (
     <div className="App">
-      <Container maxW="5xl" padding="10">
-        <TopNavbar />
-        <Body />
-      </Container>
+      <Body />
     </div>
   );
 }
--- src/App/Components/Body.js
+++ src/App/Components/Body.js
@@ -1,15 +1,5 @@
 import React from "react";
-import { Box, SimpleGrid } from "@chakra-ui/react";
-import TutCard from "./TutCard";
 
 export default function Body() {
-  return (
-    <div className="content">
-      <SimpleGrid columns={3} spacing={10}>
-        <TutCard />
-        <TutCard />
-        <TutCard />
-      </SimpleGrid>
-    </div>
-  );
+  return <div className="content">Hello</div>;
 }
