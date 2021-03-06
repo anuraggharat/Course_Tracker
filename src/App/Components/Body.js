3264500a5bc7b08a6a74f0a0ec76dace5f3dbe69
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-03-06 20:32:35 +0530

    netlify functions added

--- src/App/Components/Body.js
+++ src/App/Components/Body.js
@@ -1,20 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTut from "./AddTut";
 import TutCard from "./TutCard";
 import { IoMdAdd } from "react-icons/io";
 
 export default function Body() {
   const [modal, setModal] = useState(false);
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const toggle = () => setModal(!modal);
+
+  const loadCourses = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/courses");
+      const courses = await res.json();
+      console.log(courses);
+      setCourses(courses);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
   return (
     <div className="container">
       <AddTut toggle={toggle} modal={modal} />
       <div className="row pt-4">
-        <TutCard />
-        <TutCard />
-
-        <TutCard />
+        {!loading && courses.map((item) => <TutCard item={item} />)}
       </div>
       <button
         className="btn float-btn btn-primary rounded-0 d-flex align-items-center"
