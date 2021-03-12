1f6d719f231c2acf4b8591dafc05ce164cdc9019
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-03-13 00:12:53 +0530

    PWA features added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App/App";
 import "react-toastify/dist/ReactToastify.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+serviceWorkerRegistration.register();
